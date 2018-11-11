4592d2f9ff2eaa07d77c84a84ef64b5a97b0872a
Roman K <kopylroman@gmail.com>
2018-11-11 21:39:40 +0200

    Transfer Products from Store to Cart

--- classes/Cart.php
+++ classes/Cart.php
@@ -4,12 +4,14 @@ namespace OOPStore;
 
 class Cart implements CartInterface
 {
+    private $store;
     private $customer;
     private $products;
     private $purchase;
 
-    public function __construct(Customer $customer)
+    public function __construct(Store $store, Customer $customer)
     {
+        $this->store = $store;
         $this->customer = $customer;
         $this->products = [];
     }
@@ -21,11 +23,20 @@ class Cart implements CartInterface
 
     public function addProduct(Product $product): bool
     {
-        if (empty($this->purchase)) {
-           $this->products[] = $product;
+        if (empty($this->purchase) && $this->store->hasProduct($product)) { //
+           $this->products[$product->getId()] = $product;
+           $this->store->removeProduct($product);
            return true;
         }
         return false;
+    }
+    
+    public function removeProduct (Product $product)
+    {
+        if (($key = array_search($product, $this->products)) !== false) {
+            unset($this->products[$key]);
+            $this->store->addProduct($product);
+        }
     }
 
     public function getTotal(): int
@@ -43,5 +54,15 @@ class Cart implements CartInterface
     {
         $this->purchase = new Purchase($this);
         return $this->purchase;
+    }
+
+    public function getProducts(): array
+    {
+        return $this->products;
+    }
+
+    public function hasProduct(Product $product): bool
+    {
+        return isset($this->products[$product->getId()]);
     }
 }
--- index.php
+++ index.php
@@ -20,15 +20,29 @@ function requireDir($dir)
 requireDir(INTERFACES_DIR);
 requireDir(CLASSES_DIR);
 
-$category = new Category('TV');
-$product1 = new Product($category, 'LG LX35', 1000000);
-$product2 = new Product($category, 'LG LX35', 1000000);
+$categoryTV = new Category('TV');
+$product1 = new Product($categoryTV, 'LG LX35', 1000000);
+$product2 = new Product($categoryTV, 'Samsung', 1000000);
+$product3 = new Product($categoryTV, 'Lenovo', 1000000);
+$product4 = new Product($categoryTV, 'Sony', 1000000);
+$product5 = new Product($categoryTV, 'Ericson', 1000000);
 $store = new Store();
+$cart = new Cart($store, new Customer('Roman', 'Kopyl'));
 
 $store->addProduct($product1);
-$store->addProduct($product1);
 $store->addProduct($product2);
+$store->addProduct($product3);
+$store->addProduct($product4);
+// $store->addProduct($product5);
 
 $store->removeProduct($product1);
 
+$cart->addProduct($product2);
+$cart->addProduct($product3);
+
+$cart->removeProduct($product3);
+
+$cart->addProduct($product5);
+
 print_r($store->getProducts());
+print_r($cart->getProducts());
